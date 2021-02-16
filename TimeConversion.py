4bc75dfbf723b58b512e211c70f829263099aa67
adnan500 <adnan500@my.yorku.ca>
2021-02-16 10:50:56 -0500

    Project 1

---
+++ TimeConversion.py
@@ -0,0 +1,16 @@
+# Lab 1
+# Author: Adnan Fahad Faizi
+# Email: adnan500@my.yorku.ca
+# Student ID: 217905928
+
+print("My name is Adnan Fahad Faizi")
+print("My York student ID is: 217905928")
+print(("I currently live in Brampton, Ontario, Canada"))
+print()
+number_of_hours = float(input("Enter number of hours: "))
+minutes = number_of_hours * 60
+seconds = number_of_hours * 3600
+print("Converted to minutes: ")
+print(minutes)
+print("Converted to seconds: ")
+print(seconds)
